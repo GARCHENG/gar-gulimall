414230e670bf04ee3a5c88b0ce23fe6d03aed8dd
GARCHENG <631450632@qq.com>
2023-08-16 23:20:49 +0800

    初始构建

---
+++ gulimall-coupon/src/test/java/com/garcheng/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garcheng.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-product/src/test/java/com/garcheng/gulimall/product/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garcheng.gulimall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
