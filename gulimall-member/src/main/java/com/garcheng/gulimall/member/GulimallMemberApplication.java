bdbaa46e918facdb745aba95f3504f17c300aeb8
GARCHENG <631450632@qq.com>
2023-08-17 22:52:46 +0800

    集成springcloud Alibaba nacos 注册中心

--- gulimall-member/src/main/java/com/garcheng/gulimall/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/garcheng/gulimall/member/GulimallMemberApplication.java
@@ -3,8 +3,11 @@ package com.garcheng.gulimall.member;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("com.garcheng.gulimall.member.dao")
 public class GulimallMemberApplication {
 
