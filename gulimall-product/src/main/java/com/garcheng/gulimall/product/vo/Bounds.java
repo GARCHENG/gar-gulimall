76bf971a27b980cd8b2b53c86c0f4cb4b2e7f9da
ZhongJiaCheng <zhongjiacheng@jzdrones.com>
2023-08-29 10:49:23 +0800

    发布商品vo类抽取

---
+++ gulimall-product/src/main/java/com/garcheng/gulimall/product/vo/Bounds.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright 2023 bejson.com
+ */
+package com.garcheng.gulimall.product.vo;
+
+/**
+ * Auto-generated: 2023-08-29 10:37:20
+ *
+ * @author bejson.com (i@bejson.com)
+ * @website http://www.bejson.com/java2pojo/
+ */
+public class Bounds {
+
+    private int buyBounds;
+    private int growBounds;
+
+    public void setBuyBounds(int buyBounds) {
+        this.buyBounds = buyBounds;
+    }
+
+    public int getBuyBounds() {
+        return buyBounds;
+    }
+
+    public void setGrowBounds(int growBounds) {
+        this.growBounds = growBounds;
+    }
+
+    public int getGrowBounds() {
+        return growBounds;
+    }
+
+}
