b8e2250c4a7efa8764cf30b7e75e70c46c0285ae
ZhongJiaCheng <zhongjiacheng@jzdrones.com>
2023-08-30 16:42:56 +0800

    ware模块仓库库存list

--- gulimall-ware/src/main/java/com/garcheng/gulimall/ware/service/impl/WareSkuServiceImpl.java
+++ gulimall-ware/src/main/java/com/garcheng/gulimall/ware/service/impl/WareSkuServiceImpl.java
@@ -1,5 +1,6 @@
 package com.garcheng.gulimall.ware.service.impl;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -18,9 +19,19 @@ public class WareSkuServiceImpl extends ServiceImpl<WareSkuDao, WareSkuEntity> i
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        QueryWrapper<WareSkuEntity> queryWrapper = new QueryWrapper<>();
+        String wareId = (String) params.get("wareId");
+        if (!StringUtils.isEmpty(wareId)){
+            queryWrapper.eq("ware_id",wareId);
+        }
+        String skuId = (String) params.get("skuId");
+        if (!StringUtils.isEmpty(skuId)){
+            queryWrapper.eq("sku_id",skuId);
+        }
+
         IPage<WareSkuEntity> page = this.page(
                 new Query<WareSkuEntity>().getPage(params),
-                new QueryWrapper<WareSkuEntity>()
+                queryWrapper
         );
 
         return new PageUtils(page);
