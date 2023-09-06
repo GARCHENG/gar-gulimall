77d55522807b1bb51a0604602603c6e7e6d48a8f
ZhongJiaCheng <zhongjiacheng@jzdrones.com>
2023-09-06 18:39:05 +0800

    es测试保存

--- gulimall-search/src/main/java/com/garcheng/gulimall/search/config/EsConfig.java
+++ gulimall-search/src/main/java/com/garcheng/gulimall/search/config/EsConfig.java
@@ -1,17 +1,31 @@
 package com.garcheng.gulimall.search.config;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.client.HttpAsyncResponseConsumerFactory;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class EsConfig {
 
+    public static final RequestOptions COMMON_OPTIONS;
+    static {
+        RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
+//        builder.addHeader("Authorization", "Bearer " + TOKEN);
+//        builder.setHttpAsyncResponseConsumerFactory(
+//                new HttpAsyncResponseConsumerFactory
+//                        .HeapBufferedResponseConsumerFactory(30 * 1024 * 1024 * 1024));
+        COMMON_OPTIONS = builder.build();
+    }
+
+    @Bean
     public RestHighLevelClient esRestClient(){
         RestHighLevelClient client = new RestHighLevelClient(
                 RestClient.builder(
-                        new HttpHost("192.168.137.155", 9200, "http")));
+                        new HttpHost("192.168.137.158", 9200, "http")));
         return client;
     }
 }
--- gulimall-search/src/test/java/com/garcheng/gulimall/search/GulimallSearchApplicationTests.java
+++ gulimall-search/src/test/java/com/garcheng/gulimall/search/GulimallSearchApplicationTests.java
@@ -1,16 +1,40 @@
 package com.garcheng.gulimall.search;
 
+import com.garcheng.gulimall.search.config.EsConfig;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GulimallSearchApplicationTests {
 
+    @Autowired
+    private RestHighLevelClient client;
+
     @Test
-    public void contextLoads() {
+    public void contextLoads() throws IOException {
+
+        IndexRequest request = new IndexRequest("posts");
+        request.id("2");
+        String jsonString = "{" +
+                "\"user\":\"garcheng\"," +
+                "\"postDate\":\"2013-01-30\"," +
+                "\"message\":\"trying out Elasticsearch\"" +
+                "}";
+        request.source(jsonString, XContentType.JSON);
+
+        IndexResponse indexResponse = client.index(request, EsConfig.COMMON_OPTIONS);
+        System.out.println(indexResponse);
     }
 
 }
