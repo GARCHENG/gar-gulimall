384b938a831b0e03bb6b18b2e79d4408051b4e3d
ZhongJiaCheng <zhongjiacheng@jzdrones.com>
2023-09-15 17:02:17 +0800

    检索服务-检索条件抽取

--- gulimall-search/src/main/java/com/garcheng/gulimall/search/web/SearchController.java
+++ gulimall-search/src/main/java/com/garcheng/gulimall/search/web/SearchController.java
@@ -1,13 +1,20 @@
 package com.garcheng.gulimall.search.web;
 
+import com.garcheng.gulimall.search.service.MallSearchService;
+import com.garcheng.gulimall.search.vo.SearchParams;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SearchController {
 
+
+    @Autowired
+    private MallSearchService mallSearchService;
+
     @RequestMapping("list.html")
-    public String listPage(){
+    public String listPage(SearchParams searchParams){
         return "list";
     }
 
