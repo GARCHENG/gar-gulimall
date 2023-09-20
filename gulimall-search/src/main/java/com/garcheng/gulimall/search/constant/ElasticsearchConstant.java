8b2b03e41a4472c8e0024fdbde7f68c380eac416
ZhongJiaCheng <zhongjiacheng@jzdrones.com>
2023-09-20 14:26:12 +0800

    检索服务-前端页面基本渲染

--- gulimall-search/src/main/java/com/garcheng/gulimall/search/constant/ElasticsearchConstant.java
+++ gulimall-search/src/main/java/com/garcheng/gulimall/search/constant/ElasticsearchConstant.java
@@ -4,5 +4,5 @@ public interface ElasticsearchConstant {
 
     static final String PRODUCT_INDEX_NAME = "gulimall_product";
 
-    static final Integer PRODUCT_SEARCH_PAGE_SIZE = 2;
+    static final Integer PRODUCT_SEARCH_PAGE_SIZE = 12;
 }
