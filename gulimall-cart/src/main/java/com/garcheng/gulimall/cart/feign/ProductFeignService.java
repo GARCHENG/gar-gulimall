9ce86376c9c08db3fdf74d1f78177e2f04896eaa
ZhongJiaCheng <zhongjiacheng@jzdrones.com>
2023-10-08 18:46:19 +0800

    购物车模块-添加购物车

---
+++ gulimall-cart/src/main/java/com/garcheng/gulimall/cart/feign/ProductFeignService.java
@@ -0,0 +1,20 @@
+package com.garcheng.gulimall.cart.feign;
+
+import com.garcheng.gulimall.common.utils.R;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@FeignClient("gulimall-product")
+public interface ProductFeignService {
+
+    @RequestMapping("product/skuinfo/info/{skuId}")
+    R info(@PathVariable("skuId") Long skuId);
+
+    @GetMapping("product/skusaleattrvalue/getSaleAttrString/{skuId}")
+    List<String> getSaleAttrString(@PathVariable("skuId") Long skuId);
+
+}
