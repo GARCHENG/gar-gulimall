4f248f78347650738154b9a04df3b08235445950
ZhongJiaCheng <zhongjiacheng@jzdrones.com>
2023-10-09 11:32:24 +0800

    购物车模块-添加购物车细节处理

--- gulimall-cart/src/main/java/com/garcheng/gulimall/cart/service/impl/CartServiceImpl.java
+++ gulimall-cart/src/main/java/com/garcheng/gulimall/cart/service/impl/CartServiceImpl.java
@@ -10,6 +10,7 @@ import com.garcheng.gulimall.cart.vo.SkuInfoEntity;
 import com.garcheng.gulimall.cart.vo.UserInfoTo;
 import com.garcheng.gulimall.common.utils.R;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.BoundHashOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -36,32 +37,47 @@ public class CartServiceImpl implements CartService {
     @Override
     public CartItem addToCart(Long skuId, Integer num) throws ExecutionException, InterruptedException {
         BoundHashOperations<String, Object, Object> cartOps = getCartOps();
-        CartItem cartItem = new CartItem();
 
-        CompletableFuture<Void> getSkuInfoTask = CompletableFuture.runAsync(() -> {
-            R result = productFeignService.info(skuId);
-            if (result.getCode() == 0){
-                SkuInfoEntity skuInfo = result.getData("skuInfo", new TypeReference<SkuInfoEntity>() {});
-                cartItem.setSkuId(skuId);
-                cartItem.setTitle(skuInfo.getSkuTitle());
-                cartItem.setImage(skuInfo.getSkuDefaultImg());
-                cartItem.setPrice(skuInfo.getPrice());
-                cartItem.setCount(num);
-            }else {
-                log.error("远程调用product模块失败。。。。");
-            }
-        }, executor);
+        String res = (String) cartOps.get(skuId.toString());
+        if (StringUtils.isEmpty(res)){
+            //添加新商品到购物车
+            CartItem cartItem = new CartItem();
+            CompletableFuture<Void> getSkuInfoTask = CompletableFuture.runAsync(() -> {
+                R result = productFeignService.info(skuId);
+                if (result.getCode() == 0){
+                    SkuInfoEntity skuInfo = result.getData("skuInfo", new TypeReference<SkuInfoEntity>() {});
+                    cartItem.setSkuId(skuId);
+                    cartItem.setTitle(skuInfo.getSkuTitle());
+                    cartItem.setImage(skuInfo.getSkuDefaultImg());
+                    cartItem.setPrice(skuInfo.getPrice());
+                    cartItem.setCount(num);
+                }else {
+                    log.error("远程调用product模块失败。。。。");
+                }
+            }, executor);
 
-        CompletableFuture<Void> getSaleAttrTask = CompletableFuture.runAsync(() -> {
-            List<String> saleAttrString = productFeignService.getSaleAttrString(skuId);
-            cartItem.setSkuAttr(saleAttrString);
-        }, executor);
+            CompletableFuture<Void> getSaleAttrTask = CompletableFuture.runAsync(() -> {
+                List<String> saleAttrString = productFeignService.getSaleAttrString(skuId);
+                cartItem.setSkuAttr(saleAttrString);
+            }, executor);
 
-        CompletableFuture.allOf(getSkuInfoTask,getSaleAttrTask).get();
+            CompletableFuture.allOf(getSkuInfoTask,getSaleAttrTask).get();
 
-        String jsonString = JSON.toJSONString(cartItem);
-        cartOps.put(skuId+"",jsonString);
-        return cartItem;
+            String jsonString = JSON.toJSONString(cartItem);
+            cartOps.put(skuId+"",jsonString);
+
+            return cartItem;
+        }else {
+            //只修改数量
+            CartItem cartItem = JSON.parseObject(res, CartItem.class);
+            cartItem.setCount(cartItem.getCount()+num);
+            String json = JSON.toJSONString(cartItem);
+            cartOps.put(skuId+"",json);
+
+            return cartItem;
+        }
+
+
     }
 
     private BoundHashOperations<String, Object, Object> getCartOps() {
