93e4d126941b4a435a2562137975f7a40a825726
ZhongJiaCheng <zhongjiacheng@jzdrones.com>
2023-10-10 11:55:34 +0800

    rabbitmq-AmqpAdmin,RabbitTemplate使用+配置json对象转换器（发送消息时自动将对象转化为json对象）

--- gulimall-order/src/test/java/com/garcheng/gulimall/order/GulimallOrderApplicationTests.java
+++ gulimall-order/src/test/java/com/garcheng/gulimall/order/GulimallOrderApplicationTests.java
@@ -1,13 +1,59 @@
 package com.garcheng.gulimall.order;
 
+import com.alibaba.fastjson.JSON;
+import com.garcheng.gulimall.order.entity.OrderEntity;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@Slf4j
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class GulimallOrderApplicationTests {
+public class GulimallOrderApplicationTests {
+
+    @Autowired
+    AmqpAdmin amqpAdmin;
+    @Autowired
+    RabbitTemplate rabbitTemplate;
 
     @Test
-    void contextLoads() {
+    public void createExchange() {
+        //String name, boolean durable, boolean autoDelete, Map<String, Object> arguments
+        DirectExchange exchange = new DirectExchange("hello-java-exchange",true,false,null);
+        amqpAdmin.declareExchange(exchange);
+        log.info("交换机【{}】,创建成功....","hello-java-exchange");
     }
 
+
+    @Test
+    public void createQueue(){
+        //String name, boolean durable, boolean exclusive, boolean autoDelete, Map<String, Object> arguments
+        Queue queue = new Queue("hello-java-queue",true,false,false,null);
+        amqpAdmin.declareQueue(queue);
+        log.info("队列【{}】,创建成功....","hello-java-queue");
+    }
+
+    @Test
+    public void bingExchangeAndQueue(){
+        //String destination, DestinationType destinationType, String exchange, String routingKey,
+        //			Map<String, Object> arguments
+        Binding bing = new Binding("hello-java-queue", Binding.DestinationType.QUEUE,"hello-java-exchange","hello.java",null);
+        amqpAdmin.declareBinding(bing);
+        log.info("绑定【{}】,创建成功....","hello-java-binging");
+    }
+
+    @Test
+    public void sendMessage(){
+        //发送的消息是对象，会使用序列化机制，需实现ser...
+        //String exchange, String routingKey, Object message
+        OrderEntity orderEntity = new OrderEntity();
+        orderEntity.setBillContent("hello");
+        rabbitTemplate.convertAndSend("hello-java-exchange","hello.java",orderEntity);
+        log.info("消息发送完成：{}", JSON.toJSONString(orderEntity));
+    }
 }
