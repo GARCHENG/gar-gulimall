d5a2732d44727883992ae8a2c008942e831d4d97
ZhongJiaCheng <zhongjiacheng@jzdrones.com>
2023-10-12 15:54:03 +0800

    订单模块-订单确认页模拟运费

--- gulimall-ware/src/main/java/com/garcheng/gulimall/ware/controller/WareInfoController.java
+++ gulimall-ware/src/main/java/com/garcheng/gulimall/ware/controller/WareInfoController.java
@@ -1,15 +1,12 @@
 package com.garcheng.gulimall.ware.controller;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.Map;
 
 //import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.garcheng.gulimall.ware.entity.WareInfoEntity;
 import com.garcheng.gulimall.ware.service.WareInfoService;
@@ -39,6 +36,12 @@ public class WareInfoController {
         PageUtils page = wareInfoService.queryPage(params);
 
         return R.ok().put("page", page);
+    }
+
+    @GetMapping("fare")
+    public R getFare(@RequestParam("addrId") Long addrId){
+        BigDecimal fare = wareInfoService.getFare(addrId);
+        return R.ok().setData(fare);
     }
 
 
--- gulimall-ware/src/main/java/com/garcheng/gulimall/ware/service/impl/WareInfoServiceImpl.java
+++ gulimall-ware/src/main/java/com/garcheng/gulimall/ware/service/impl/WareInfoServiceImpl.java
@@ -1,7 +1,14 @@
 package com.garcheng.gulimall.ware.service.impl;
 
+import com.alibaba.fastjson.TypeReference;
+import com.garcheng.gulimall.common.utils.R;
+import com.garcheng.gulimall.ware.feign.MemberFeignService;
+import com.garcheng.gulimall.ware.vo.MemberAddressVo;
 import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
@@ -16,6 +23,9 @@ import com.garcheng.gulimall.ware.service.WareInfoService;
 
 @Service("wareInfoService")
 public class WareInfoServiceImpl extends ServiceImpl<WareInfoDao, WareInfoEntity> implements WareInfoService {
+
+    @Autowired
+    MemberFeignService memberFeignService;
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -36,6 +46,17 @@ public class WareInfoServiceImpl extends ServiceImpl<WareInfoDao, WareInfoEntity
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public BigDecimal getFare(Long addrId) {
+        R data = memberFeignService.info(addrId);
+        MemberAddressVo memberAddressVo = data.getData("memberReceiveAddress", new TypeReference<MemberAddressVo>() {});
+        if (memberAddressVo != null) {
+            String phone = memberAddressVo.getPhone();
+            return new BigDecimal(phone.substring(phone.length()-2,phone.length()));
+        }
+        return null;
     }
 
 }
