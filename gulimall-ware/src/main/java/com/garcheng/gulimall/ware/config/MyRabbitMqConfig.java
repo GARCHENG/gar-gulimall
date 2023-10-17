ef777716d8090a8a3dace32cac1b3f788ee89b10
ZhongJiaCheng <zhongjiacheng@jzdrones.com>
2023-10-17 18:29:43 +0800

    订单服务-创建业务交换机队列等

---
+++ gulimall-ware/src/main/java/com/garcheng/gulimall/ware/config/MyRabbitMqConfig.java
@@ -0,0 +1,83 @@
+package com.garcheng.gulimall.ware.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@Slf4j
+public class MyRabbitMqConfig {
+
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    //在MyRabbitMqConfig初始化完成之后再调用此方法
+    @PostConstruct
+    public void initRabbitTemplate() {
+        rabbitTemplate.setConfirmCallback(new RabbitTemplate.ConfirmCallback() {
+            @Override
+            public void confirm(CorrelationData correlationData, boolean ack, String cause) {
+                log.info("arrived mq server：CorrelationData【{}】,Ack【{}】,Cause【{}】", correlationData, ack, cause);
+            }
+        });
+
+        rabbitTemplate.setReturnCallback(new RabbitTemplate.ReturnCallback() {
+            @Override
+            public void returnedMessage(Message message, int replyCode, String replyText, String exchange, String routingKey) {
+                log.error("Message【{}】==>ReplyCode【{}】==>ReplyText【{}】==>Exchange【{}】==>RoutingKey【{}】", message, replyCode, replyText, exchange, routingKey);
+            }
+        });
+    }
+
+    @Bean
+    public Exchange stockEventExchange() {
+        //String name, boolean durable, boolean autoDelete, Map<String, Object> arguments
+        TopicExchange topicExchange = new TopicExchange("stock-event-exchange", true, false, null);
+        return topicExchange;
+    }
+
+    @Bean
+    public Queue stockDelayQueue() {
+        Map<String, Object> args = new HashMap<>();
+        args.put("x-dead-letter-exchange", "stock-event-exchange");
+        args.put("x-dead-letter-routing-key", "stock.release");
+        args.put("x-message-ttl", 120000);
+        return new Queue("stock.delay.queue", true, false, false, args);
+    }
+
+    @Bean
+    public Queue stockReleaseStockQueue(){
+        return new Queue("stock.release.stock.queue", true, false, false);
+    }
+
+    @Bean
+    public Binding stockReleaseBinding(){
+        //String destination, DestinationType destinationType, String exchange, String routingKey,
+        //			Map<String, Object> arguments
+        return new Binding("stock.release.stock.queue", Binding.DestinationType.QUEUE,"stock-event-exchange","stock.release.#",null);
+    }
+
+    @Bean
+    public Binding stockLockedBinding(){
+        //String destination, DestinationType destinationType, String exchange, String routingKey,
+        //			Map<String, Object> arguments
+        return new Binding("stock.delay.queue", Binding.DestinationType.QUEUE,"stock-event-exchange","stock.locked",null);
+    }
+
+
+}
